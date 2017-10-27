2d46855465ddb7e8523ef17a5a26c24e0105cc9e
Jiang Juzhi <toe.jiang@gmail.com>
2017-10-28 00:47:37 +0800

    add zoom-in animation when photoswipe open.
    use jquery the find the img object, and get position and width for zoom-in/zoom-out animation.
    wrap ember-photoswipe in gallery-photoswipe to make code reusable

---
+++ app/components/gallery-photoswipe.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  actions: {
+    open(photoswipe, index, hash) {
+      hash = hash || {};
+      hash.index = index;
+      hash.getThumbBoundsFn = function getThumbBoundsFn(index) {
+        var items = this.get('items'),
+          item = items ? items[index] : null,
+          id = item ? item.info.id : 0,
+          ele = this.$('#'+id),
+          pos = ele ? ele.position() : {left:0,top:0},
+          width = ele ? ele.width() : 0;
+        return {x:pos.left, y:pos.top, w:width};
+      }.bind(this);
+      photoswipe.actions.open(hash);
+    },
+  },
+});
--- app/helpers/is-equal.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function isEqual(params/*, hash*/) {
-  let [p1,p2] = params;
-  return p1 === p2;
-}
-
-export default Ember.Helper.helper(isEqual);
