9374847e335bac4232e70357008d044534098423
Jiang Juzhi <toe.jiang@gmail.com>
2017-10-31 15:50:34 +0800

    add additional buttons to top-bar if using http://github.com/ailinked/ember-photoswipe.git.
    this ember-photoswipe repo is forked to add support of additional buttons, like a button to show detail info of the photo, or a 'like this' button

--- app/components/gallery-photoswipe.js
+++ app/components/gallery-photoswipe.js
@@ -1,21 +1,38 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  showHideOpacity: false,
+  bgOpacity: 0.9,
+  barsSize: {top:20, bottom:'auto'},
   itemProperties: ['src', 'w', 'h', 'msrc'],
+  timeToIdle: 1200,
+
+  additionalButtons: [
+    {
+      name:'detail',
+      title:'Photo detail',
+      option: 'closeEl',
+      onTap: function onTap() {
+        Ember.Logger.info('detail button taped');
+      },
+    },
+  ],
 
   actions: {
+    getThumbBoundsFn: function getThumbBoundsFn(index) {
+      var items = this.get('items'),
+        item = items ? items[index] : null,
+        id = item ? item.info.id : 0,
+        ele = this.$('#'+id),
+        pos = ele ? ele.position() : {left:0,top:0},
+        width = ele ? ele.width() : 0;
+      return {x:pos.left, y:pos.top, w:width};
+    },
+
     open(photoswipe, index, hash) {
       hash = hash || {};
       hash.index = index;
-      hash.getThumbBoundsFn = function getThumbBoundsFn(index) {
-        var items = this.get('items'),
-          item = items ? items[index] : null,
-          id = item ? item.info.id : 0,
-          ele = this.$('#'+id),
-          pos = ele ? ele.position() : {left:0,top:0},
-          width = ele ? ele.width() : 0;
-        return {x:pos.left, y:pos.top, w:width};
-      }.bind(this);
       photoswipe.actions.open(hash);
     },
   },
